49301d033d14463e993599f87e56700059da2572
Ana Vitoria <anavitoriacosta.29@gmail.com>
2026-02-25 14:29:05 -0300

    Criando prisma client

---
+++ src/routes/main.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { prisma } from '../libs/prisma';
+
+
+export const mainRouter = Router();
+
+mainRouter.get('/ping', (req, res) => {
+    res.json({ pong: true });
+});
+
+mainRouter.get('/ping',(req,res)=>{
+res.json({pong: true})
+})
