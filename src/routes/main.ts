d703fdfbd55119a57686d667b2f335dc93b23d60
Ana Vitoria <anavitoriacosta.29@gmail.com>
2026-03-05 14:41:03 -0300

    0403

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
-import { prisma } from '../libs/prisma';
+import {
+    createUser,
+    createUsers,
+    getAllUsers, 
+    getUserByEmail
+} from '../services/user'
 
 
 export const mainRouter = Router();
@@ -8,6 +13,55 @@ mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
 });
 
-mainRouter.get('/ping',(req,res)=>{
-res.json({pong: true})
+mainRouter.get('/ping', (req, res) => {
+    res.json({ pong: true })
+})
+
+mainRouter.get('/test', (req, res) => {
+    res.json({ testando: true });
+})
+
+mainRouter.post('/user', async (req, res) => {
+    const user = await createUser({
+        data: {
+            name: 'anny',
+            email: 'anny@gmail.com',
+            status: true,
+            posts: {
+                create: {
+                    title: 'Post1 - Anny',
+                    body: 'Firts post by Anny'
+                }
+            }
+        }
+    });
+    if (user) {
+        res.status(201).json({ user })
+    } else {
+        res.status(400).json({ error: 'Email already exists' })
+    }
+})
+
+mainRouter.post('/users', async (req, res) => {
+    const users = await createUsers([]);
+    res.status(201).json({ ok: true })
+});
+
+
+mainRouter.get("/users", async (req, res) => {
+  const users = await getAllUsers();
+  if (users) {
+    res.json({ users });
+  } else {
+    res.status(500).json({ error: "Error fetching users" });
+  }
+});
+
+mainRouter.get('/user', async(req, res)=>{
+  const user = await getUserByEmail ('anny@gmail.com');
+  if(user){
+    res.json ({user})
+  } else {
+    res.status(404).json({error:'user not found'})
+  }
 })
---
+++ src/services/user.ts
@@ -0,0 +1,69 @@
+import { Prisma } from "@prisma/client";
+import { prisma } from "../libs/prisma";
+
+type CreateUserProps = {
+  name: string;
+  email: string;
+};
+
+export const createUser = async ({data}: {data: Prisma.UserCreateInput}) => {
+  try {
+
+    return await prisma.user.create({ data })
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+            console.error('Email already exists:');
+            return false;
+        }
+    }
+    console.error("Error creating user:", error);
+    return false;
+  }
+};
+
+export const createUsers = async (users: Prisma.UserCreateInput[]) => {
+    const result = await prisma.user.createMany({
+        data: [
+                {name: 'João', email: 'joao@example.com'}, 
+                {name: 'João 2', email: 'joao2@example.com'}, 
+                {name: 'Maria', email: 'maria@example.com'},
+                {name: 'Pedro', email: 'pedro@example.com'} 
+        ],
+        skipDuplicates: true
+    })
+}
+
+export const getAllUsers = async () => {
+    try {
+      return await prisma.user.findMany({
+        select:{
+          id: true,
+          name: true,
+          email: true,
+          status: true
+        }
+      })
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      return false;
+    }
+}
+
+export const getUserByEmail = async (email: string) =>{
+  try {
+    return await prisma.user.findUnique ({
+      where:{ email},
+      select:{
+        id: true,
+        name: true,
+        status: true
+      }
+      
+    })
+  }catch (error){
+    console.error('Error fatching user by email', error)
+    return false;
+  }
+}
+
